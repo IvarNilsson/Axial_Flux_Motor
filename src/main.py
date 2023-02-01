567d10451f4af1d8505a6f6a93016282c1f5043f
IvarNilsson <ivar.j.nilsson@gmail.com>
2023-02-01 20:13:14 +0100

    v0.1

--- src/main.py
+++ src/main.py
@@ -9,8 +9,61 @@ AG = Pin(5, Pin.OUT)
 BG = Pin(6, Pin.OUT)
 CG = Pin(7, Pin.OUT)
 
+
+def off_all():
+    AV.value(1)
+    BV.value(1)
+    CV.value(1)
+    AG.value(0)
+    BG.value(0)
+    CG.value(0)
+
+def coil_1_2():
+    AV.value(0)
+    BG.value(1)
+    
+def coil_1_3():
+    AV.value(0)
+    CG.value(1)
+
+def coil_2_3():
+    BV.value(0)
+    CG.value(1)
+
+def coil_2_1():
+    BV.value(0)
+    AG.value(1)
+
+def coil_3_1():
+    CV.value(0)
+    AG.value(1)
+
+def coil_3_2():
+    CV.value(0)
+    BG.value(1)
+
 while True:
-    led.value(1)
+
+    off_all()
+    coil_1_2()
     time.sleep(0.5)
-    led.value(0)
+
+    off_all()
+    coil_1_3()
+    time.sleep(0.5)
+
+    off_all()
+    coil_2_3()
+    time.sleep(0.5)
+
+    off_all()
+    coil_2_1()
+    time.sleep(0.5)
+
+    off_all()
+    coil_3_1()
+    time.sleep(0.5)
+
+    off_all()
+    coil_3_2()
     time.sleep(0.5)
