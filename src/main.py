aa0af2a3e2310f1e2f4ae39d31a76020a323070d
IvarNilsson <ivar.j.nilsson@gmail.com>
2023-02-02 10:20:32 +0100

    added function to check for shorts

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@ AG = Pin(5, Pin.OUT)
 BG = Pin(6, Pin.OUT)
 CG = Pin(7, Pin.OUT)
 
+states = [1, 1, 1, 0, 0, 0]  # [0]=AV, [1]=BV, [2]=CV, [3]=AG, [4]=BG, [5]=CG
+
 
 def off_all():
     AV.value(1)
@@ -17,30 +19,69 @@ def off_all():
     AG.value(0)
     BG.value(0)
     CG.value(0)
+    states[0] = 1
+    states[1] = 1
+    states[2] = 1
+    states[3] = 0
+    states[4] = 0
+    states[5] = 0
+
 
 def coil_1_2():
     AV.value(0)
     BG.value(1)
-    
+    states[0] = 0
+    states[4] = 1
+
+
 def coil_1_3():
     AV.value(0)
     CG.value(1)
+    states[0] = 0
+    states[5] = 1
+
 
 def coil_2_3():
     BV.value(0)
     CG.value(1)
+    states[1] = 0
+    states[5] = 1
+
 
 def coil_2_1():
     BV.value(0)
     AG.value(1)
+    states[1] = 0
+    states[3] = 1
+
 
 def coil_3_1():
     CV.value(0)
     AG.value(1)
+    states[2] = 0
+    states[3] = 1
+
 
 def coil_3_2():
     CV.value(0)
     BG.value(1)
+    states[2] = 0
+    states[4] = 1
+
+
+def check_short():  # check if combination shorts the circuit
+    short = 0
+    if (states[0] == 0 and states[3] == 1):
+        print("short! 1")
+        short = 1
+    elif (states[1] == 0 and states[4] == 1):
+        print("short! 2")
+        short = 1
+    elif (states[2] == 0 and states[5] == 1):
+        print("short! 3")
+        short = 1
+    return short
+
 
 while True:
 
