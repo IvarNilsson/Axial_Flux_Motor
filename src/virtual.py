8af2bb9880e9d4ee6f377f4221ebe0facb8f60fd
IvarNilsson <ivar.j.nilsson@gmail.com>
2023-02-02 11:09:28 +0100

    built a virtual simulatior that can run the code to check for shotsts.etc

--- src/virtual.py
+++ src/virtual.py
@@ -1,72 +1,85 @@
-from machine import Pin
 import time
 
-led = Pin(25, Pin.OUT)
-AV = Pin(2, Pin.OUT)
-BV = Pin(3, Pin.OUT)
-CV = Pin(4, Pin.OUT)
-AG = Pin(5, Pin.OUT)
-BG = Pin(6, Pin.OUT)
-CG = Pin(7, Pin.OUT)
+led = 0
+AV = 0
+BV = 0
+CV = 0
+AG = 0
+BG = 0
+CG = 0
 
 states = [1, 1, 1, 0, 0, 0]  # [0]=AV, [1]=BV, [2]=CV, [3]=AG, [4]=BG, [5]=CG
 
 
 def off_all():
-    AV.value(1)
-    BV.value(1)
-    CV.value(1)
-    AG.value(0)
-    BG.value(0)
-    CG.value(0)
+    AV = 1
+    BV = 1
+    CV = 1
+    AG = 0
+    BG = 0
+    CG = 0
     states[0] = 1
     states[1] = 1
     states[2] = 1
     states[3] = 0
     states[4] = 0
     states[5] = 0
+    if (seems_good() != 2):
+        print("ERROR0", seems_good())
 
 
 def coil_1_2():
-    AV.value(0)
-    BG.value(1)
+    AV = 0
+    BG = 1
     states[0] = 0
     states[4] = 1
+    if (seems_good() != 1):
+        print("ERROR1", seems_good())
 
 
 def coil_1_3():
-    AV.value(0)
-    CG.value(1)
+    AV = 0
+    CG = 1
     states[0] = 0
     states[5] = 1
+    if (seems_good() != 1):
+        print("ERROR2", seems_good())
 
 
 def coil_2_3():
-    BV.value(0)
-    CG.value(1)
+    BV = 0
+    CG = 1
     states[1] = 0
     states[5] = 1
+    if (seems_good() != 1):
+        print("ERROR3", seems_good())
 
 
 def coil_2_1():
-    BV.value(0)
-    AG.value(1)
+    BV = 0
+    AG = 1
     states[1] = 0
     states[3] = 1
+    if (seems_good() != 1):
+        print("ERROR4", seems_good())
 
 
 def coil_3_1():
-    CV.value(0)
-    AG.value(1)
+    CV = 0
+    AG = 1
     states[2] = 0
     states[3] = 1
+    if (seems_good() != 1):
+        print("ERROR5", seems_good())
 
 
 def coil_3_2():
-    CV.value(0)
-    BG.value(1)
+    CV = 0
+    BG = 1
     states[2] = 0
     states[4] = 1
+    if (seems_good() != 1):
+        print("ERROR6", seems_good())
 
 
 def check_short():  # check if combination shorts the circuit
@@ -106,7 +119,23 @@ def check_coils():  # checks which coil combination is "open"    WARNING: this d
     return coils
 
 
-while True:
+def seems_good():  # auto checks (0=bad 1=good 2=off)
+    short = check_short()
+    if (short == 1):
+        return 0
+
+    coils = check_coils()
+    if (coils[0] == 0 and coils[1] == 0 and coils[2] == 0):
+        return 2
+    elif ((coils[0] + coils[1] + coils[2]) != 0):
+        print("????")
+        return 0
+    else:
+        return 1
+
+
+for i in range(5):
+    print("loop", i)
 
     off_all()
     coil_1_2()
