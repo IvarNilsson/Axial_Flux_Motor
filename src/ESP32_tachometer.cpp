c41f3b034c30802c640d8ccbce14d511863d6626
IvarNilsson <ivar.j.nilsson@gmail.com>
2023-05-14 17:37:22 +0200

    small fix but still problems with hall-sensor

--- src/ESP32_tachometer.cpp
+++ src/ESP32_tachometer.cpp
@@ -3,7 +3,7 @@
 #include <Arduino.h>           //standarad
 #include <Wire.h>              //display
 
-#define HALL_D_PIN 19
+#define HALL_D_PIN 33
 
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
